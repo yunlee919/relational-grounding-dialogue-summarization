972e9db8113e2eb0f166479d49011eaedf282566
Yunwoo Lee <yunu919@gmail.com>
2026-07-03 02:32:17 -0700

    Add

--- src/groundlm/__init__.py
+++ src/groundlm/__init__.py
@@ -1,3 +1,3 @@
-"""GroundLM: Dialogue-Semantic Grounding Error Taxonomy for cross-lingual dialogue summarization."""
+"""grounded-semantic-pipeline package for cross-lingual dialogue summarization analysis."""
 
 __version__ = "0.1.0"
